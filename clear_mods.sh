6429485427fa1c097d6df84e2e2dd5da741c2665
BK-Hsu <yuki820113@gmail.com>
2025-12-02 15:37:47 +0800

    Clean root contents for new layout

--- clear_mods.sh
+++
@@ -1,3 +0,0 @@
-rmmod mods 2>/dev/null
-modprobe -r nvidia_modeset nvidia_uvm nvidia_drm nvidia
-lsof | grep /dev/nvidia
--- delete_tmux.sh
+++
@@ -1,1 +0,0 @@
-tmux list-sessions | awk '{print $1}' | sed 's/://g' | xargs -I {} tmux kill-session -t {}
